26191a33b2165013266e08806a3b412eaa52f143
Yang, Aiden <yunfany3@illinois.edu>
2021-09-18 13:14:57 +0800

    mp2 initial commit

---
+++ mp2/geometry.py
@@ -0,0 +1,202 @@
+# geometry.py
+# ---------------
+# Licensing Information:  You are free to use or extend this projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to the University of Illinois at Urbana-Champaign
+#
+# Created by James Gao (jamesjg2@illinois.edu) on 9/03/2021
+# Inspired by work done by Jongdeog Lee (jlee700@illinois.edu)
+
+"""
+This file contains geometry functions necessary for solving problems in MP2
+"""
+
+import math
+import numpy as np
+from alien import Alien
+
+def does_alien_touch_wall(alien, walls,granularity):
+    """Determine whether the alien touches a wall
+
+        Args:
+            alien (Alien): Instance of Alien class that will be navigating our map
+            walls (list): List of endpoints of line segments that comprise the walls in the maze in the format [(startx, starty, endx, endx), ...]
+            granularity (int): The granularity of the map
+
+        Return:
+            True if touched, False if not
+    """
+    return False
+
+def does_alien_touch_goal(alien, goals):
+    """Determine whether the alien touches a goal
+        
+        Args:
+            alien (Alien): Instance of Alien class that will be navigating our map
+            goals (list): x, y coordinate and radius of goals in the format [(x, y, r), ...]. There can be multiple goals
+        
+        Return:
+            True if a goal is touched, False if not.
+    """
+    return False
+
+def is_alien_within_window(alien, window,granularity):
+    """Determine whether the alien stays within the window
+        
+        Args:
+            alien (Alien): Alien instance
+            window (tuple): (width, height) of the window
+            granularity (int): The granularity of the map
+    """
+    return True
+
+if __name__ == '__main__':
+    #Walls, goals, and aliens taken from Test1 map
+    walls =   [(0,100,100,100),  
+                (0,140,100,140),
+                (100,100,140,110),
+                (100,140,140,130),
+                (140,110,175,70),
+                (140,130,200,130),
+                (200,130,200,10),
+                (200,10,140,10),
+                (175,70,140,70),
+                (140,70,130,55),
+                (140,10,130,25),
+                (130,55,90,55),
+                (130,25,90,25),
+                (90,55,90,25)]
+    goals = [(110, 40, 10)]
+    window = (220, 200)
+
+    #Initialize Aliens and perform simple sanity check. 
+    alien_ball = Alien((30,120), [40, 0, 40], [11, 25, 11], ('Horizontal','Ball','Vertical'), 'Ball', window)	
+    assert not does_alien_touch_wall(alien_ball, walls, 0), f'does_alien_touch_wall(alien, walls) with alien config {alien_ball.get_config()} returns True, expected: False'
+    assert not does_alien_touch_goal(alien_ball, goals), f'does_alien_touch_goal(alien, walls) with alien config {alien_ball.get_config()} returns True, expected: False'
+    assert is_alien_within_window(alien_ball, window, 0), f'is_alien_within_window(alien, walls) with alien config {alien_ball.get_config()} returns False, expected: True'
+
+    alien_horz = Alien((30,120), [40, 0, 40], [11, 25, 11], ('Horizontal','Ball','Vertical'), 'Horizontal', window)	
+    assert not does_alien_touch_wall(alien_horz, walls, 0), f'does_alien_touch_wall(alien, walls) with alien config {alien_horz.get_config()} returns True, expected: False'
+    assert not does_alien_touch_goal(alien_horz, goals), f'does_alien_touch_goal(alien, walls) with alien config {alien_horz.get_config()} returns True, expected: False'
+    assert is_alien_within_window(alien_horz, window, 0), f'is_alien_within_window(alien, walls) with alien config {alien_horz.get_config()} returns False, expected: True'
+
+    alien_vert = Alien((30,120), [40, 0, 40], [11, 25, 11], ('Horizontal','Ball','Vertical'), 'Vertical', window)	
+    assert does_alien_touch_wall(alien_vert, walls, 0),f'does_alien_touch_wall(alien, walls) with alien config {alien_vert.get_config()} returns False, expected: True'
+    assert not does_alien_touch_goal(alien_vert, goals), f'does_alien_touch_goal(alien, walls) with alien config {alien_vert.get_config()} returns True, expected: False'
+    assert is_alien_within_window(alien_vert, window, 0), f'is_alien_within_window(alien, walls) with alien config {alien_vert.get_config()} returns False, expected: True'
+
+    edge_horz_alien = Alien((50, 100), [100, 0, 100], [11, 25, 11], ('Horizontal','Ball','Vertical'), 'Horizontal', window)
+    edge_vert_alien = Alien((200, 70), [120, 0, 120], [11, 25, 11], ('Horizontal','Ball','Vertical'), 'Vertical', window)
+
+    def test_helper(alien : Alien, position, truths):
+        alien.set_alien_pos(position)
+        config = alien.get_config()
+        assert does_alien_touch_wall(alien, walls, 0) == truths[0], f'does_alien_touch_wall(alien, walls) with alien config {config} returns {not truths[0]}, expected: {truths[0]}'
+        assert does_alien_touch_goal(alien, goals) == truths[1], f'does_alien_touch_goal(alien, goals) with alien config {config} returns {not truths[1]}, expected: {truths[1]}'
+        assert is_alien_within_window(alien, window, 0) == truths[2], f'is_alien_within_window(alien, window) with alien config {config} returns {not truths[2]}, expected: {truths[2]}'
+
+    alien_positions = [
+                        #Sanity Check
+                        (0, 100),
+
+                        #Testing window boundary checks
+                        (25.6, 25.6),
+                        (25.5, 25.5),
+                        (194.4, 174.4),
+                        (194.5, 174.5),
+
+                        #Testing wall collisions
+                        (30, 112),
+                        (30, 113),
+                        (30, 105.5),
+                        (30, 105.6), # Very close edge case
+                        (30, 135),
+                        (140, 120),
+                        (187.5, 70), # Another very close corner case, right on corner
+                        
+                        #Testing goal collisions
+                        (110, 40),
+                        (145.5, 40), # Horizontal tangent to goal
+                        (110, 62.5), # ball tangent to goal
+                        
+                        #Test parallel line oblong line segment and wall
+                        (50, 100),
+                        (200, 100),
+                        (205.5, 100) #Out of bounds
+                    ]
+
+    #Truths are a list of tuples that we will compare to function calls in the form (does_alien_touch_wall, does_alien_touch_goal, is_alien_within_window)
+    alien_ball_truths = [
+                            (True, False, False),
+                            (False, False, True),
+                            (False, False, True),
+                            (False, False, True),
+                            (False, False, True),
+                            (True, False, True),
+                            (False, False, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (False, True, True),
+                            (False, False, True),
+                            (True, True, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True)
+                        ]
+    alien_horz_truths = [
+                            (True, False, False),
+                            (False, False, True),
+                            (False, False, False),
+                            (False, False, True),
+                            (False, False, False),
+                            (False, False, True),
+                            (False, False, True),
+                            (True, False, True),
+                            (False, False, True),
+                            (True, False, True),
+                            (False, False, True),
+                            (True, False, True),
+                            (True, True, True),
+                            (False, True, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, False),
+                            (True, False, False)
+                        ]
+    alien_vert_truths = [
+                            (True, False, False),
+                            (False, False, True),
+                            (False, False, False),
+                            (False, False, True),
+                            (False, False, False),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (False, False, True),
+                            (True, True, True),
+                            (False, False, True),
+                            (True, True, True),
+                            (True, False, True),
+                            (True, False, True),
+                            (True, False, True)
+                        ]
+
+    for i in range(len(alien_positions)):
+        test_helper(alien_ball, alien_positions[i], alien_ball_truths[i])
+        test_helper(alien_horz, alien_positions[i], alien_horz_truths[i])
+        test_helper(alien_vert, alien_positions[i], alien_vert_truths[i])
+
+    #Edge case coincide line endpoints
+    test_helper(edge_horz_alien, edge_horz_alien.get_centroid(), (True, False, False))
+    test_helper(edge_horz_alien, (110,55), (True, True, True))
+    test_helper(edge_vert_alien, edge_vert_alien.get_centroid(), (True, False, True))
+
+
+    print("Geometry tests passed\n")
